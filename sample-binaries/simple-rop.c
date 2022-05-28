aa95fb0cc89e001322b1f25408c887c5e6041d88
tifui-alexandru <itifui00@gmail.com>
2022-05-28 16:32:12 +0300

    new soruce code

--- sample-binaries/simple-rop.c
+++ sample-binaries/simple-rop.c
@@ -5,11 +5,18 @@
 int main(int argc, char** argv) {
     printf("Try to hack me!\n");
     
+    FILE* fd = fopen(argv[1], "rb");
+
+    fseek (fd , 0 , SEEK_END);
+    size_t total_bytes = ftell(fd);
+    rewind(fd);
+
     char buffer[8];
-    size_t total_bytes = atoi(argv[1]);
-    read(0, buffer, total_bytes);
+    fread(buffer, 1, total_bytes, fd);
     
     printf("Fail!\n");
+
+    printf(buffer);
 
     return 0;
 }
